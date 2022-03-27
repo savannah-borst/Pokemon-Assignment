933d4f27aac89b2ac21aceeddb63ca3452cd96a3
savannah-borst <savannah.borst@gmail.com>
2022-03-27 17:41:00 +0200

    setting up pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPage } from './pages/landing/landing.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPage,
+    TrainerPage,
+    CataloguePage,
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/catalogue/catalogue.page.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CataloguePage } from './catalogue.page';
+
+describe('CataloguePage', () => {
+  let component: CataloguePage;
+  let fixture: ComponentFixture<CataloguePage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CataloguePage ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CataloguePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
